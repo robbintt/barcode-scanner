dd4a0e408aa91e90a811c7864c2cfae2aa31f5dc
Noisebridge StandingDesk <noisebridge-standingdesk@noisebridge.net>
2015-11-13 13:58:45 -0800

    Moved statements and updated outputs.

--- bookscanner.py
+++ bookscanner.py
@@ -4,8 +4,9 @@ Goal: Dump barcodes.
 """
 import sqlite3
 import os
-import sys
 import time
+import pprint
+
 
 OUTPUT_DIR = "data"
 SQLITE_FILE = "barcodes.sqlite"
@@ -45,7 +46,12 @@ if __name__ == "__main__":
     print "Please SAVE your work OFTEN, there is no error handling."
     print "   * * *"
 
-    INPUT_STATEMENT = "Type 'save' to save and continue.\nType 'quit' to exit.\nPlease scan the next book:"
+    SAVE_STATEMENT = "Type 'save' to save and continue.\n"
+    DUMP_STATEMENT = "Type 'dump' to see the barcodes ready to be saved\n"
+    QUIT_STATEMENT = "Type 'quit' to exit.\n"
+    INSTRUCTION_STATEMENT = "Please scan the next book:"
+    INPUT_STATEMENT = SAVE_STATEMENT + DUMP_STATEMENT + \
+                        QUIT_STATEMENT + INSTRUCTION_STATEMENT
 
 
     barcodes = list()
@@ -61,6 +67,11 @@ if __name__ == "__main__":
         # capture 'quit'
         if _inputline == "quit":
             continue
+        if _inputline == "dump":
+            print "Current barcodes NOT YET SAVED:"
+            pprint.pprint(barcodes)
+            _inputline = ""
+            continue
         if _inputline == "save":
             print "   * * *"
             print sqlite_save(barcodes)
@@ -74,7 +85,7 @@ if __name__ == "__main__":
         print "   * * *"
         print "Barcode has {} numbers.".format(len(_inputline))
 
-        _accept_inputline = raw_input("Accept barcode? (press enter, any key to discard):")
+        _accept_inputline = raw_input("Accept barcode? (enter to accept, type ANYTHING to discard):")
         if _accept_inputline.lower() == "":
             barcodes.append(_inputline)
             print "Barcode: STORED. There are now {} unsaved barcodes.".format(len(barcodes))
@@ -84,10 +95,10 @@ if __name__ == "__main__":
             print "Barcode: DISCARDED DISCARDED DISCARDED."
             time.sleep(1)
             print "\a"
-            print "Barcode: DISCARDED DISCARDED DISCARDED."
+            print "Discard cooldown. 2 seconds remaining."
             time.sleep(1)
             print "\a"
-            print "Barcode: DISCARDED DISCARDED DISCARDED."
+            print "Discard cooldown. 1 second remaining."
             time.sleep(1)
             print "   * * *"
 
