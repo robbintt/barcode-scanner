af24f6e7efe78c7f46906116361ea89fef165afd
Trent Robbins <robbintt@gmail.com>
2015-11-18 00:14:24 -0800

    Refined concept of when to paginate.

--- bookscanner.py
+++ bookscanner.py
@@ -11,15 +11,19 @@ import pprint
 OUTPUT_DIR = "data"
 SQLITE_FILE = "barcodes.sqlite"
 
-LINE_NUMBER = 0
+PAGE_NUMBER = 0
 SPACER = " " * 40 # can use any character, empty seems to be best
 
 def numbered_spacer():
-    global LINE_NUMBER
-    LINE_NUMBER += 1
-    return "-{} (Page # {})".format(SPACER, str(LINE_NUMBER))
+    global PAGE_NUMBER
+    PAGE_NUMBER += 1
+    pagination =  "-{} (Page # {})".format(SPACER, str(PAGE_NUMBER))
+    pagination += "\n"
+    pagination += " {} type 'help' for commands".format(SPACER,)
 
-def construct_input_statement(*args):
+    return pagination
+
+def construct_statement(*args):
     """ dead simple statement to keep control flow clean.
 
     This is broken out so that more interesting things
@@ -77,15 +81,21 @@ if __name__ == "__main__":
     SHOW_STATEMENT = "Type 'show' to see the barcodes ready to be saved\n"
     HYPERSCAN_STATEMENT = "Type 'hyperscan' to enter fast mode.\n"
     UNDO_STATEMENT = "Type 'undo' to undo the last entry.\n"
+    HELP_STATEMENT = "Type 'help' to see this help.\n"
     QUIT_STATEMENT = "Type 'quit' to exit.\n"
+    WHY_PAGES = "There is a page # to help the human eye track changes."
+
     INSTRUCTION_STATEMENT = "Please scan the NEXT BOOK:"
 
-    INPUT_STATEMENT = construct_input_statement(SAVE_STATEMENT, 
+
+    COMMAND_INFORMATION = construct_statement(SAVE_STATEMENT, 
             SHOW_STATEMENT, HYPERSCAN_STATEMENT, UNDO_STATEMENT, 
-            QUIT_STATEMENT, INSTRUCTION_STATEMENT)
+            HELP_STATEMENT, QUIT_STATEMENT + WHY_PAGES)
+
+    INPUT_STATEMENT = construct_statement(INSTRUCTION_STATEMENT)
 
 
-    print numbered_spacer()
+    print
     print "Please SAVE your work OFTEN, there is no error handling."
     print numbered_spacer()
 
@@ -99,6 +109,12 @@ if __name__ == "__main__":
         # capture empty values, for good UX
         if _inputline == "":
             print numbered_spacer()
+            continue
+
+        if _inputline == "help":
+            print COMMAND_INFORMATION
+            print numbered_spacer()
+            _inputline == ""
             continue
 
         if _inputline == "hyperscan":
@@ -129,29 +145,27 @@ if __name__ == "__main__":
         if _inputline == "quit":
             continue
         if _inputline == "show":
-            print numbered_spacer()
             print "Current barcodes NOT YET SAVED:"
             for barcode in barcodes:
                 print barcode
-            print numbered_spacer()
             _inputline = ""
+            print numbered_spacer()
             continue
         if _inputline == "save":
             print sqlite_save(barcodes)
             barcodes = list() # start fresh
-            print numbered_spacer()
             print "There are now {} unsaved barcodes.".format(len(barcodes))
             print numbered_spacer()
             _inputline = ""
             continue
 
-        print numbered_spacer()
         print
         print "Barcode has {} numbers.".format(len(_inputline))
         print
 
         if HYPERSCAN_TOGGLE == False:
             print "accept: Press ENTER.  |  discard: type anything THEN press enter."
+            print
             _accept_inputline = raw_input("ACCEPT scan?")
         else:
             # skip confirmation if hyperscan is on/True
@@ -159,7 +173,6 @@ if __name__ == "__main__":
 
         if _accept_inputline == "":
             barcodes.append(_inputline)
-            print numbered_spacer()
             print "Barcode: STORED."
             print "There are now {} unsaved barcodes.".format(len(barcodes))
             print numbered_spacer()
